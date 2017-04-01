6e3fec8e5614de525ac2856f1ca3b22be18a27e6
galgend <ggendler@gmail.com>
2017-04-01 13:48:08 +0300

    added logs

--- location/location.controller.js
+++ location/location.controller.js
@@ -2,6 +2,7 @@ var locationsService = require('./location.service');
 var baseController = require('../common/base.controller')
 
 let autocompleteCities=(res, req)=>{
+	console.log('Autocomplete request ');
     var text = req.param('text'); 
 	if (typeof text === undefined)
     baseController.handleBadRequestError("autocomplete search text wasn't found")
