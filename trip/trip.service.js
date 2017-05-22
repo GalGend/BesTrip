fa3e67b645c07c2049707c86b919be3070a86e4e
galgend <ggendler@gmail.com>
2017-05-22 23:32:01 +0300

    Added the functionality to add a new trip to the db- still needs to validate in the trip controllers that all params are valid- example that the dates are valid, that the accomodtion place id exits etc etc

--- trip/trip.service.js
+++ trip/trip.service.js
@@ -1,6 +1,7 @@
 var dbAccess = require('../common/db-access')
 var Trip = dbAccess.models.trip;
 var Promise = require('bluebird');
+var locationService = require('../location/location.service')
 
 var getTripById=function(mongoId){
     return Trip.findById(mongoId, {name:1, 
@@ -19,7 +20,36 @@ return Trip.find({_id:dbAccess.tools.getIdObject(tripId),
 
 }
 
+var saveNewTrip = function(tripObj){
+	var trip  = new Trip({name:tripObj.name,
+		user: tripObj.user,
+		dates:tripObj.dates, 
+		accomodation:{
+			accomodationPlaceId:accomodationPlaceId
+		}, 
+		tripPlan:{}
+	})
+
+	// Getting all the missing params:
+	// Data for the accomodation
+	return locationService.getSiteById(tripObj.accomodationPlaceId)
+	.then((hotelSite)=>{
+		// Setting the accomodation on the new trip
+		trip.accomodation.accomodationLocation = hotelSite.location,
+		trip.accomodation.accomodationName  = hotelSite.name
+
+		return trip.save();
+	})
+
+}
+
+var updateTripPlan = function(tripId, tripPlan){
+	return Trip.findOneAndUpdate()
+}
 module.exports={
     getTripById:getTripById,
-    getTripDayByIndex:getTripDayByIndex
+    getTripDayByIndex:getTripDayByIndex,
+	saveNewTrip :saveNewTrip,
+	updateTripPlan:updateTripPlan
+	
 }
